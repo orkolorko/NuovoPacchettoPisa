4aaa2830d1aeea272dd95a4b1eeda36ed39eca97
Documenter.jl <documenter@juliadocs.github.io>
2024-06-26 13:44:02 +0000

    build based on 5ecc2ae

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NuovoPacchettoPisa","category":"page"},{"location":"#NuovoPacchettoPisa","page":"Home","title":"NuovoPacchettoPisa","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NuovoPacchettoPisa.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NuovoPacchettoPisa]","category":"page"}]
+}
